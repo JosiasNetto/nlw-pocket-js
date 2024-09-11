84fb6cd606fbd5cc365ecaabb8ed554c2d909509
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-09-11 18:34:42 -0300

    fundamentos 001

---
+++ index.js
@@ -0,0 +1,9 @@
+//Hello World
+let message = "Hello world!"
+console.log(message);
+
+message = "Hi"
+console.log(message);
+
+const msg2 = "World Hello!" //can`t change the value
+console.log(msg2);
