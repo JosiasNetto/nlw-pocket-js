4a404a53fd0a5a232e9976bda76a536fca14cc4c
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-09-12 12:53:17 -0300

    lista metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const  { select, input }  = require('@inquirer/prompts')
+const  { select, input, checkbox }  = require('@inquirer/prompts')
 
 let metas = []
 
@@ -12,6 +12,35 @@ const cadastrar_meta = async () => {
 
   metas.push({value: meta, checked: false})
 
+}
+
+const listar_metas = async () => {
+  const respostas = await checkbox({
+    message: "Use as setas para mudar de meta, o Espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false
+  })
+
+  metas.forEach((m) => {
+    m.checked = false
+  })
+  
+  if(!respostas.length) {
+    console.log("Nenhuma meta selecionada!")
+    return
+  }
+
+  
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta
+    })
+
+    meta.checked = true
+
+  })
+
+  console.log("Meta(s) marcada(s) como concluida(s)")
 }
 
 const start = async () => {
@@ -42,7 +71,7 @@ const start = async () => {
         console.log(metas)
         break
       case "listar":
-        console.log("vamos listar")
+        await listar_metas()
         break
       case "sair":
         console.log("Ate a proxima!")
