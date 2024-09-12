b14e4ddded62e087d1544df0b998dcd2f1a9324d
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-09-12 10:13:41 -0300

    menu app

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const start = () => {
-  let count = 0
-  while(count < 10){
-    console.log(count)
-    count++ 
+const  { select }  = require('@inquirer/prompts')
+
+const start = async () => {
+
+  while(true){
+
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar"
+        },
+        {
+          name: "Listar metas",
+          value: "listar"
+        },
+        {
+          name:"Sair",
+          value:"sair"
+        }
+      ]
+    })
+
+    switch(opcao){
+      case "cadastrar":
+        console.log("vamos cadastrar")
+        break
+      case "listar":
+        console.log("vamos listar")
+        break
+      case "sair":
+        console.log("Ate a proxima!")
+        return
+    }
   }
 }
 
